58595a9ff8eaec9efd8dde6e8fae9771fda06706
lrmiguel <leo.rmiguel@gmail.com>
2021-07-29 22:54:16 -0300

    test: :white_check_mark: Add math division test

--- tests/test_game.py
+++ tests/test_game.py
@@ -4,3 +4,12 @@ def test_soma():
     soma = a + b
 
     assert soma == 260, "should be 260"
+
+
+def test_division():
+    a = 200
+    b = 120
+
+    total = a / b
+
+    assert round(total, 1) == 1.7
